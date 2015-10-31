0b8bfeedd789ab47e317882cd64935382b38638f
Jackson <blackflag@skilstak.sh>
2015-10-31 19:20:00 -0400

    save

---
+++ py/funky.py
@@ -0,0 +1,21 @@
+"""ALL MY FUNCTIONS EVER"""
+import skilstak colors as c
+import                      
+
+
+
+
+def multi():
+    """Hello World Las Vegas Style Baby"""
+    while True:
+        print(c.clear + c.multi("Hello World"))
+        t.sleep(.1)
+
+def color():
+    """fills screen with buttyful hellow worlds""" 
+    print(c.rc() + "Hello World" + c.reset, end=" ")
+
+def hello():
+    """Prints a random hello world"""
+    print(c.clear + c.rc + c.reset +"Hello World")
+
